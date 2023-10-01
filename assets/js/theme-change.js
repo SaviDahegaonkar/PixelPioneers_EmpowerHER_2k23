5b31f3908afa01155acb8a5a86ee8b4234280188
apurva-honrao <apurva.22111216@viit.ac.in>
2023-10-01 11:28:53 +0530

    search update

--- assets/js/theme-change.js
+++ assets/js/theme-change.js
@@ -1,6 +1,7 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const currentTheme = localStorage.getItem('theme');
 
+
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme);
   
